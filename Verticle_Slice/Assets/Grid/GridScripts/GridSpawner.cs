3590c5151b6d583ee00643abd99bf155c0c5ed67
yairme <yairstward2013@gmail.com>
2021-12-01 15:28:33 +0100

    Grid system, with Tilemap to draw map.

---
+++ Verticle_Slice/Assets/Grid/GridScripts/GridSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpawner : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void GridLoader()
+    {
+
+        for (int i = 0; i < 0; i++)
+        {
+            for (int j = 0; j < 1; j++)
+            {
+
+            }
+        }
+    }
+}
