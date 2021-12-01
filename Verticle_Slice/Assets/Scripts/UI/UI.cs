86a4514b99601b1f2240925d2ec9efb44bb288f2
Daniel <danielnijkamp2004@gmail.com>
2021-12-01 10:47:04 +0100

    Added keyboard hotbar input

--- Verticle_Slice/Assets/Scripts/UI/UI.cs
+++ Verticle_Slice/Assets/Scripts/UI/UI.cs
@@ -1,20 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System;
+using System.Collections.Generic;
 using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 
 
 public class UI : MonoBehaviour
 {
-    public static float timer;
+    
     public TextMeshProUGUI time_text;
     public TextMeshProUGUI date_text;
     public int currentItem;
 
+    
+
     public List<Button> buttons = new List<Button>();
+    private KeyCode[] InputList = new KeyCode[]
+    {
+        KeyCode.Alpha1,
+        KeyCode.Alpha2,
+        KeyCode.Alpha3,
+        KeyCode.Alpha4,
+        KeyCode.Alpha5,
+        KeyCode.Alpha6,
+        KeyCode.Alpha7,
+        KeyCode.Alpha8,
+        KeyCode.Alpha9,
+        KeyCode.Alpha0,
+        KeyCode.Minus,
+        KeyCode.Equals
+
+    };
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,26 +42,70 @@ public class UI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+        InputUpdate();
+    }
+    void InputUpdate()
+    {
+        if (!Input.anyKeyDown)
+            return;
+
+        foreach (KeyCode key in InputList)
+            if (Input.GetKeyDown(key))
+                switch (key)
+                {
+                    case KeyCode.Alpha1:
+                        currentItem = 0;
+                        break;
+                    case KeyCode.Alpha2:
+                        currentItem = 1;
+                        break;
+                    case KeyCode.Alpha3:
+                        currentItem = 2;
+                        break;
+                    case KeyCode.Alpha4:
+                        currentItem = 3;
+                        break;
+                    case KeyCode.Alpha5:
+                        currentItem = 4;
+                        break;
+                    case KeyCode.Alpha6:
+                        currentItem = 5;
+                        break;
+                    case KeyCode.Alpha7:
+                        currentItem = 6;
+                        break;
+                    case KeyCode.Alpha8:
+                        currentItem = 7;
+                        break;
+                    case KeyCode.Alpha9:
+                        currentItem = 8;
+                        break;
+                    case KeyCode.Alpha0:
+                        currentItem = 9;
+                        break;
+                    case KeyCode.Minus:
+                        currentItem = 10;
+                        break;
+                    case KeyCode.Equals:
+                        currentItem = 11;
+                        break;
+                }
+        
     }
     private void OnGUI()
     {
         time_text.text = string.Format(DateTime.Now.ToString("HH:m tt"));
         date_text.text = string.Format($"{DateTime.Now.DayOfWeek.ToString().Substring(0,3)}. {DateTime.Now.Day.ToString()}");
-        
-        
     }
-    public int ButtonGetPosition(int index)
-    {
-        return index;
-    }
+    
     public void ButtonPressed(Button button)
     {
         if (buttons.Contains(button))
         {
             int index = buttons.IndexOf(button);
-            print(button.gameObject.name);
-            ButtonGetPosition(index);
+            currentItem = buttons.IndexOf(button);
+            
         }
     }
+    
 }
